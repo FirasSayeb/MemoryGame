c8159abb31ab9bf869eccedd706704f2b8bdbc99
FirasSayeb <firassayeb2@gmail.com>
2024-08-30 10:34:53 +0100

    fix matching

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 let score=0;
 let nclick=0;
 let choices=[];
-let matchs=[];
 const title=document.createElement("h1");
 title.id="title";
 const textnode = document.createTextNode("Score : "+score);
@@ -15,25 +14,30 @@ function changeImage(id){
    console.log(elem); 
    if(id!= "image1" && id!= "image3" && id!= "image5" && id!= "image7" && id!= "image9"){
     elem.src="images/image1.jfif";
-    choices.push("images/image1.jfif");
+    choices.push(elem.id);
    }else{
     elem.src="images/image2.jfif";
-    choices.push("images/image2.jfif");
+    choices.push(elem.id);
    }
    if(nclick%2==0){  
       console.log(choices);
       console.log(nclick);
-    if(choices[choices.length-2]==choices[choices.length-1]){ 
-      matchs.push(choices[choices.length-2],choices[choices.length-1]);
+    if(document.getElementById(choices[choices.length-2]).src==document.getElementById(choices[choices.length-1]).src){ 
       score+=1;
       document.getElementById('title').textContent = "Score :"+score;
        console.log(score); 
     }else{
 
+            
             let imgs=document.getElementsByTagName('img');
             for(k=0;k<imgs.length;k++){
-               if(imgs[k].src==choices[choices.length-2] || imgs[k].src==choices[choices.length-1]){
-                 imgs[k].src="images/images.jfif";
+               console.log(imgs[k].src); 
+              
+               if(imgs[k].id==choices[choices.length-2] || imgs[k].id==choices[choices.length-1] ){
+                  console.log(imgs[k].src);
+                  imgs[k].src="images/images.jfif";
+                  console.log(imgs[k].src);
+                }
                }
             }
          
@@ -41,7 +45,7 @@ function changeImage(id){
        
       
     }
-   }
+   
   
 
 
